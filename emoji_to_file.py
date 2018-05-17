b286074e4b96c24975641a9f95a94e631e6d1b1b
whanke <32702537+whanke@users.noreply.github.com>
2018-05-17 18:19:26 +0200

    streaming bag of #s

--- emoji_to_file.py
+++ emoji_to_file.py
@@ -1,18 +1,25 @@
-# writes emoji as aliases and pictures to a txt file named "twython-keywords.txt" 
+# writes emoji as aliases and pictures to a txt file named "twython-keywords.txt"
 # using in twitter stream to filter tweets
 
 import emoji
 
+
 def write_aliases_to_file(filename):
-	for e in emoji.EMOJI_UNICODE: 
-		with open(filename, "a+", encoding = "utf-8") as f:
-			f.write(e + "\n")
+    for e in emoji.EMOJI_UNICODE:
+        with open(filename, "a+", encoding="utf-8") as f:
+            f.write(e + "\n")
 
 
 def write_emoji_pic_to_file(filename):
-	for e in emoji.EMOJI_UNICODE:
-		emoji_pic = emoji.emojize(e)
-		with open(filename, "a+", encoding = "utf-8") as f:
-			f.write(emoji_pic + "\n")
+    # print also all symbolic letters as emoji
+    for e in emoji.EMOJI_UNICODE:
+        emoji_pic = emoji.emojize(e)
+        with open(filename, "a+", encoding="utf-8") as f:
+            f.write(emoji_pic + "\n")
+
+
+def convert_alias_to_emoji_pic():
+    for alias in emoji.EMOJI_UNICODE:
+
 
 write_emoji_pic_to_file("twython-keywords.txt")
--- twitter_stream.py
+++ twitter_stream.py
@@ -11,10 +11,6 @@ import codecs
 import time
 from datetime import date
 
-consumer_key = ""
-consumer_secret = ""
-access_key = ""
-access_secret = ""
 
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -34,46 +30,46 @@ class CustomStreamListener(tweepy.StreamListener):
         new_date = date.today()
         if not new_date == old_date:
             outfile.close
-            outfile = codecs.open ("tweets-" + str(new_date) + ".txt", "ab", "utf-8")
-            writer = csv.writer(outfile,quoting=csv.QUOTE_NONNUMERIC,lineterminator='\n')
+            outfile = codecs.open("tweets-" + str(new_date) + ".txt", "ab", encoding="utf-8")
+            writer = csv.writer(outfile, quoting=csv.QUOTE_NONNUMERIC, lineterminator='\n')
             old_date = new_date
         try:
-            lang = langid.classify(status.text)[0];
+            lang = langid.classify(status.text)[0]
             if lang == "en":
-                writer.writerow( (status.created_at, status.id_str, status.text) )
-        except Exception, e:
+                if not status.text.startswith('RT'):
+                    writer.writerow((status.created_at, status.id_str, status.in_reply_to_user_id_str, status.text))
+        except Exception:
             # Catch any unicode errors while printing to console
             # and just ignore them to avoid breaking application.
             sys.exc_clear()
 #                pass
 
-
     def on_error(self, status_code):
-        logfile.write(str(time.asctime( time.localtime(time.time()) )) + ' Encountered error with status code:' + str(status_code) + "\n")
-        return True # Don't kill the stream
+        logfile.write(str(time.asctime(time.localtime(time.time()))) + ' Encountered error with status code:' + str(status_code) + "\n")
+        return True  # Don't kill the stream
 
     def on_timeout(self):
-        logfile.write(str(time.asctime( time.localtime(time.time()) )) + ' Timeout...' + "\n")
-        return True # Don't kill the stream
+        logfile.write(str(time.asctime(time.localtime(time.time()))) + ' Timeout...' + "\n")
+        return True  # Don't kill the stream
 
     # def on_data(self, data):
     #     print data
     #     return True
 
 
-
-localtime = time.asctime( time.localtime(time.time()) )
-logfile.write( localtime + " Tracking terms from ../twython-keywords.txt\nStarting stream \n")
+localtime = time.asctime(time.localtime(time.time()))
+logfile.write(localtime + " Tracking terms from ../twython-keywords.txt\nStarting stream \n" + str([line.strip() for line in open('twython-keywords.txt', encoding="utf-8")]))
 
 # longer timeout to keep SSL connection open even when few tweets are coming in
 stream = tweepy.streaming.Stream(auth, CustomStreamListener(), timeout=1000.0)
 
-terms = [line.strip() for line in open('twython-keywords.txt')]
+terms = [line.strip() for line in open('twython-keywords.txt', encoding="utf-8")]
+emoji_check = []
 
 # open output file
 old_date = date.today()
-outfile = codecs.open ("tweets-" + str(old_date) + ".txt", "ab", "utf-8")
-writer = csv.writer(outfile,quoting=csv.QUOTE_NONNUMERIC,lineterminator='\n')
- 
+outfile = codecs.open("tweets-" + str(old_date) + ".txt", "ab", "utf-8")
+writer = csv.writer(outfile, quoting=csv.QUOTE_NONNUMERIC, lineterminator='\n')
+
 
 stream.filter(track=terms)
