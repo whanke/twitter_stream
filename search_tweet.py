840fc22bd90aa58c45105521f2dcc8f09d4e92f4
whanke <32702537+whanke@users.noreply.github.com>
2018-05-18 15:03:05 +0200

    access to API

--- search_tweet.py
+++ search_tweet.py
@@ -1,12 +1,13 @@
 # get a original tweet by a reply_id of a tweet
 #
 from twitter_oauth import access_key, access_secret, consumer_key, consumer_secret
+import tweepy
 
-twitter = Twython(
-    consumer_key, consumer_secret,
-    access_key, access_secret)
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_key, access_secret)
+api = tweepy.API(auth)
 
-id_of_tweet = "899143901927268352"
+id_of_tweet = "473447672"
 
-tweet = twitter.show_status(id=id_of_tweet)
-print(tweet['text'])
+tweet = api.get_status(id_of_tweet)
+print(tweet.text)
